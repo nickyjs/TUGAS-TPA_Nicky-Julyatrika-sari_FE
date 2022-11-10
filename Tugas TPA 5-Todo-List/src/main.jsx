dc82965c12e4f7ce1d49ad0bccb19969d7c32729
Nicky <nickyjulyas03@gmail.com>
2022-11-10 11:33:26 +0700

    Menambah kode baru

--- Tugas TPA 5-Todo-List/src/main.jsx
+++ Tugas TPA 5-Todo-List/src/main.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
